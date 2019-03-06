4883a62777dad287b049b5d642db162273bb8109
Caio Petrelli Cominato <caiopetrellicominato@gmail.com>
2019-03-06 18:34:18 -0300

    Added TLS

--- example_mqtt.py
+++ example_mqtt.py
@@ -1,33 +1,56 @@
+#-*- coding: utf-8 -*-
+
+#System Stuff
 import time
-from common_utils.mqtt import mqtt_connection
+import ssl
+#Libs
+import paho.mqtt.client as mqttClient
 
-#MQTT connection details
-client_id = ''
-usr = 'test'
-passw = 'test'
-host = 'localhost'
-port = '1883'
-topic = 'test'
+#This class handles MQTT connections
+class mqtt_connection:
+    def __init__(self, client_id, address, port, usr, pwd, topic, on_message, cert=None):
+        self.Connected = False                  #global variable for the state of the connection
 
-#function to act upon message
-def on_message(client, userdata, message):
-    print('Got a message at '+topic+', it reads: '+message.payload)
+        self.broker_address= address            #Broker address
+        self.port = port                        #Broker port
+        self.user = usr                         #Connection username
+        self.password = pwd                     #Connection password
+        self.client_id = client_id              #Client id
+        self.topic = topic                      #Topic
+        self.on_message = on_message
+        self.client = mqttClient.Client(self.client_id)                     #create new instance
+        self.client.username_pw_set(self.user, password=self.password)      #set username and password
+        self.client.on_connect= self._on_connect                            #attach function to connection established call
+        self.client.on_disconnect= self._on_disconnect                      #attach function to connection lost call
+        self.client.on_message= on_message                                  #attach function to callback
+        if certificate is not None:					    #attach tls if given a certificate
+            self.client.tls_set(cert, tls_version=ssl.PROTOCOL_TLSv1_2)
+        self.client.connect(self.broker_address, port=self.port)          #connect to broker
+        self.client.loop_start()                                          #start the loop
 
-#Start MQTT
-mqtt = mqtt_connection(client_id,host,port,usr,passw,topic,on_message)
+        while self.Connected != True:    #Wait for connection
+            time.sleep(0.1)
+    
+    def publish_to_topic(self,msg):
+        if self.Connected:
+            self.client.publish(self.topic,msg)
+        else:
+            print('client is not connected')
 
-#Publishing to topic
-mqtt.publish_to_topic('Hello from Python!')
-
-#Keeps main script from closing
-try:
-    while True:
-    	time.sleep(0.1)
-        imp = str(raw_input("mqtt > "))
-        mqtt.publish_to_topic(imp)
-except KeyboardInterrupt:
-	print ''
-	print "Stopping Services..."
-
-#cleans up connection
-mqtt.clean_up()
+    def _on_disconnect(self, client, userdata, rc):
+        self.Connected = False
+        if rc != 0:
+                print ("Unexpected MQTT disconnection. Attempting to reconnect.")
+                self.Connected = False
+                        
+    def _on_connect(self, client, userdata, flags, r):
+        if r == 0:
+            print(self.client_id + " Connected to broker")
+            self.Connected = True
+            self.client.subscribe(self.topic)
+        else:
+            print("Connection failed")
+    
+    def clean_up(self):
+        self.client.disconnect()
+        self.client.loop_stop()
