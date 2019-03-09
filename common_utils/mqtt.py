6f3bb74da0009815b69cbb56b49375004b277514
Caio Petrelli Cominato <caiopetrellicominato@gmail.com>
2019-03-09 14:32:19 -0300

    Added TLS

--- common_utils/mqtt.py
+++ common_utils/mqtt.py
@@ -2,13 +2,13 @@
 
 #System Stuff
 import time
-
+import ssl
 #Libs
 import paho.mqtt.client as mqttClient
 
 #This class handles MQTT connections
 class mqtt_connection:
-    def __init__(self, client_id, address, port, usr, pwd, topic, on_message):
+    def __init__(self, client_id, address, port, usr, pwd, topic, on_message, cert=None):
         self.Connected = False                  #global variable for the state of the connection
 
         self.broker_address= address            #Broker address
@@ -23,7 +23,8 @@ class mqtt_connection:
         self.client.on_connect= self._on_connect                            #attach function to connection established call
         self.client.on_disconnect= self._on_disconnect                      #attach function to connection lost call
         self.client.on_message= on_message                                  #attach function to callback
-        
+        if cert is not None:					                            #attach tls if given a certificate
+            self.client.tls_set(cert, tls_version=ssl.PROTOCOL_TLSv1_2)
         self.client.connect(self.broker_address, port=self.port)          #connect to broker
         self.client.loop_start()                                          #start the loop
 
@@ -39,7 +40,7 @@ class mqtt_connection:
     def _on_disconnect(self, client, userdata, rc):
         self.Connected = False
         if rc != 0:
-                print "Unexpected MQTT disconnection. Attempting to reconnect."
+                print ("Unexpected MQTT disconnection. Attempting to reconnect.")
                 self.Connected = False
                         
     def _on_connect(self, client, userdata, flags, r):
